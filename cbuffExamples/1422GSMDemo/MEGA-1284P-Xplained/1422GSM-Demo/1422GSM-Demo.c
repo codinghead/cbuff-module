7e008a9366bc8d3af0120c0d234d54a555556807
Stuart Cording <stuart.cording@atmel.com>
2012-01-18 13:55:16 +0100

    Bunch of new stuff on AVR branch
    Don't know what status here actually is. Just commiting for the time
    being.

---
+++ cbuffExamples/1422GSMDemo/MEGA-1284P-Xplained/1422GSM-Demo/1422GSM-Demo.c
@@ -0,0 +1,16 @@
+/*
+ * _1422GSM_Demo.c
+ *
+ * Created: 22.12.2011 15:33:46
+ *  Author: Stuart.Cording
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
